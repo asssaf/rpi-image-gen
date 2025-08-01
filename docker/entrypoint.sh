288e4bff8b6e1156ebb8e46eb2e9bae418828aee
Assaf Berg <asssaf@users.noreply.github.com>
2025-08-01 02:18:58 +0000

    genimage.cfg debug

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-set -eux
+set -euxo pipefail
 
 /rpi-image-gen/build.sh "$@" && exit 0
 
 echo "Build failed"
-find /rpi-image-gen/work
+
+# useful in case of "genimage error" (might also want to run genimage with a higher --loglevel)
+cat /rpi-image-gen/work/custom/artefacts/genimage.cfg

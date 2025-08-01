631326902be1630ae4786584070bea47521777e0
Assaf Berg <asssaf@users.noreply.github.com>
2025-08-01 00:16:27 +0000

    recovery partition layout

---
+++ custom/image/mbr/recovery/pre-image.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -eu
+
+rootfs=$1
+genimg_in=$2
+
+RECOVERY_LABEL="RECOVERY"
+BOOT_LABEL="BOOT"
+ROOT_LABEL="ROOT"
+HOME_LABEL="HOME"
+
+# Write genimage template
+cat genimage.cfg.in | sed \
+   -e "s|<IMAGE_DIR>|$IGconf_sys_outputdir|g" \
+   -e "s|<IMAGE_NAME>|$IGconf_image_name|g" \
+   -e "s|<IMAGE_SUFFIX>|$IGconf_image_suffix|g" \
+   -e "s|<RECOVERY_SIZE>|$IGconf_image_recovery_part_size|g" \
+   -e "s|<FW_SIZE>|$IGconf_image_boot_part_size|g" \
+   -e "s|<ROOT_SIZE>|$IGconf_image_root_part_size|g" \
+   -e "s|<HOME_SIZE>|$IGconf_image_home_part_size|g" \
+   -e "s|<SECTOR_SIZE>|$IGconf_device_sector_size|g" \
+   -e "s|<MKE2FSCONF>|'$(readlink -ef mke2fs.conf)'|g" \
+   -e "s|<RECOVERY_LABEL>|$RECOVERY_LABEL|g" \
+   -e "s|<BOOT_LABEL>|$BOOT_LABEL|g" \
+   -e "s|<ROOT_LABEL>|$ROOT_LABEL|g" \
+   -e "s|<HOME_LABEL>|$ROOT_LABEL|g" \
+   > ${genimg_in}/genimage.cfg
